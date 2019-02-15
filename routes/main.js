c7a4e7f1c62a72bd8d611b7f3b294b7040962660
sovovy <sovovy@gmail.com>
2019-02-16 08:02:23 +0900

    미리보기용 small img 생성

--- routes/main.js
+++ routes/main.js
@@ -14,6 +14,7 @@ module.exports = (app) => {
     let formidable = require('formidable');
     let fs = require('fs-extra');
     let rename = require('rename');
+    let sharp = require('sharp');
     
     let form = new formidable.IncomingForm();
     form.multiples = true; 
@@ -24,18 +25,23 @@ module.exports = (app) => {
     });
 
     form.on('end', function(field, files){
-        let temp_path = this.openedFiles[0].path;
-        let file_name = rename(this.openedFiles[0].name, function() {
-          return {suffix: '_'+Date.now()};
-        });
-        let new_location = './public/uploads/'; 
-        fs.move(temp_path, new_location + file_name, function(err){
-          if(err){
-            console.log(err);
-          }else{
-            console.log('upload success!');
-          }
-        })
+      let temp_path = this.openedFiles[0].path;
+      let file_name = rename(this.openedFiles[0].name, function() {
+        return {suffix: '_'+Date.now()};
+      });
+      let new_location = './public/uploads/'; 
+      fs.move(temp_path, new_location + file_name, function(err){
+        if(err){
+          console.log(err);
+        }else{
+          // small image generate
+          sharp(new_location + file_name)
+          .resize({width: 200})
+          .toFile(new_location + 'small/' + file_name);
+          
+          console.log('upload success!');
+        }
+      });
     });
   });
 };
